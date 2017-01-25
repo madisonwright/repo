71f6fea2ac596cfc66d061453e9421a5da1eadd9
madison <madisonw@uoregon.edu>
2017-01-25 16:20:23 +0000

    puts all data into lists ready for sending to tables

--- sql/pythonsort.py
+++ sql/pythonsort.py
@@ -7,6 +7,22 @@ print(sys.argv)
 conn = psycopg2.connect(dbname=sys.argv[1],host='127.0.0.1',port=int(sys.argv[2]))
 cur = conn.cursor()
 diction = dict()
+product_list_data = []
+acquisitions_data = []
+convoy_data = []
+DC_inventory_data = []
+HQ_inventory_data = []
+MB005_inventory_data = []
+SPNV_inventory_data = []
+NC_inventory_data = []
+security_compartments_data = []
+security_levels_data = []
+transit_data = []
+vendor_data = []
+
+
+
+
 
 
 def products():
@@ -34,27 +50,247 @@ def products():
 		print(diction)
 	return
 
-
-
-
-def make_products():
+def assets():
+	#asset_pk
 	with open('/home/osnapdev/repo/sql/osnap_legacy/product_list.csv','r') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-		for row in spamreader:
-			first = row[0]
-			first_list = first.split(',')
-			
-			print(first_list[0])
-			if first_list[0] != 'name':
-				print(first_list[0])  #here the list is slilt at we see only the name column
-				cur.execute("INSERT INTO products (description) VALUES (%s)",(first_list[0],))
-				cur.execute("SELECT product_pk FROM products WHERE description=%s",(first_list[0],))
-				diction[first_list[0]] = cur.fetchone()[0]
-	
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		data = []
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				data.append(new_row)
+				whole = ''
+			count+=1
+
+		productfk = diction()
+		for dat in data:
+			#product_fk, asset_tag, description,alt_description
+			cur.execute("INSERT INTO assets (product_fk, asset_tag, description,alt_description) VALUES (%s,%s,%s,%s)",(dat[4],dat[2],dat[0]))
+			cur.execute("SELECT asset_pk FROM assets WHERE asset_tag=%s",(dat[4],))
+			diction[dat[4],dat[2],dat[0]] = cur.fetchone()[0]
+		
+	return
+def vehicles():
+	#vehicle_pk
+	#asset_fk
+	return
+def facilities():
+	#facility_pk
+	#fcode, common_name, location
+	return
+def asset_at():
+	#asset_fk,facility_fk,arrive_dt,depart_dt
+	return
+def convoys():
+	#convoy_pk
+	#request, source_fk, dest_fk, depart2_dt, arrive2_dt
+	return
+def used_by():
+	#vehicle_fk, convoy_fk
+	return
+def asset_on():
+	#asset_fk, convoy_fk, load_dt, unload_dt
+	return
+def users():
+	#user_pk
+	#username, active
+	return
+def roles():
+	#role_pk
+	#title
+	return
+def user_is():
+	#user_fk, role_fk
+	return
+def user_supports():
+	#user_fk, facility_fk
+	return
+def levels():
+	#level_pk
+	#abbrv, comment
+	return
+def compartments():
+	#compartment_pk
+	#abbrv, comment
+	return
+def security_tags():
+	#tag_pk
+	#level_fk, compartment_fk, user_fk, products_fk, asset_fk
 	return
 
-#make_products()
-products()
+def open_files():
+
+	with open('/home/osnapdev/repo/sql/osnap_legacy/product_list.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				product_list_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/acquisitions.csv','r') as csvfile:
+			reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+			whole = ''
+			count = 0
+			for row in reader:
+				if count > 0:
+					for i in row:
+						whole += i
+						whole += " "
+					new_row = whole.split(',')
+					acquisitions_data.append(new_row)
+					whole = ''
+				count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/convoy.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				convoy_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/DC_inventory.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				DC_inventory_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/HQ_inventory.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				HQ_inventory_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/MB005_inventory.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				MB005_inventory_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/NC_inventory.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				NC_inventory_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/SPNV_inventory.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				SPNV_inventory_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/security_compartments.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				security_compartments_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/security_levels.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				security_levels_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/transit.csv','r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		whole = ''
+		count = 0
+		for row in reader:
+			if count > 0:
+				for i in row:
+					whole += i
+					whole += " "
+				new_row = whole.split(',')
+				transit_data.append(new_row)
+				whole = ''
+			count+=1
+	with open('/home/osnapdev/repo/sql/osnap_legacy/vendors.csv','r') as csvfile:
+			reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+			whole = ''
+			count = 0
+			for row in reader:
+				if count > 0:
+					for i in row:
+						whole += i
+						whole += " "
+					new_row = whole.split(',')
+					vendor_data.append(new_row)
+					whole = ''
+				count+=1
+
+	return
+
+
+
+
+
+#products()
+open_files()
+print(convoy_data,vendor_data, product_list_data)
 
 conn.commit()
 """productpk = row[0]
